b3bf1505f1120e00a1696138894e92faf3c5d99e
Gosha Vtorygin <georgiyvtor246@mail.ru>
2020-12-09 13:06:42 +0300

    fixing counting of factorial

--- iterator.py
+++ iterator.py
@@ -1,20 +1,16 @@
 import itertools
 
-def factorial(number):
-    if number == 1 or number == 0:
-        return 1
-    return number * factorial(number-1)
-
 class Factorial:
     class FactIter:
 
         def __init__(self):
             self.i = 1
+            self.cur_fact = 1
 
         def __next__(self):
-            cur_fact = factorial(self.i)
+            self.cur_fact *= self.i
             self.i += 1
-            return cur_fact
+            return self.cur_fact
 
     def __iter__(self):
         return Factorial.FactIter()
