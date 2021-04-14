79a3ec1f6eaed555a5585bd6a7ff998081a2a3b0
Gosha Vtorygin <georgiyvtor246@mail.ru>
2021-04-14 22:25:19 +0300

    adding module

--- space.py
+++ space.py
@@ -1,63 +1,8 @@
-from numbers import Number
-import math
+import module
 
-
-class ComplexNumber:
-    """поле комплексных чисел"""
-
-    def __init__(self, re, im = 0.0):
-        if isinstance(re, Number) and isinstance(im, Number):
-            self.real = re
-            self.imaginary = im
-        else:
-            raise ValueError("Impossible to create a complex number")
-        
-    def __str__(self):
-        a, b = '',''
-        if self.real != 0:
-            a = str('{:.3f}'.format(self.real))
-        if self.imaginary != 0:
-            b = ('','+')[self.imaginary > 0] + str('{:.3f}'.format(self.imaginary)) + 'i'
-        return a + b
-
-    def __abs__(self):
-        return math.sqrt(self.real*self.real + self.imaginary*self.imaginary)
-
-    def argument(self):
-        return math.atan(self.imaginary / self.real)
-
-    def conjugate(self):
-        self.imaginary *= -1
-        return self
-
-    def __neg__(self):
-        return ComplexNumber(
-            -self.real, -self.imaginary 
-            )
-    
-    def __add__(self, other):
-        if isinstance(other, ComplexNumber) == False:
-            other = ComplexNumber(other)
-        return ComplexNumber(
-            self.real +  other.real, self.imaginary + other.imaginary 
-            )
-
-    def __radd__(self, other):
-        return self.__add__(other)
-
-    def __mul__(self, other):
-        if isinstance(other, ComplexNumber) == False:
-            other = ComplexNumber(other)
-        return ComplexNumber(
-            self.real*other.real - self.imaginary*other.imaginary, self.real*other.imaginary + self.imaginary*other.real
-            )
-    
-    def __rmul__(self, other):
-        return self.__mul__(other)
-
-c1 = ComplexNumber(3, 4)
-c2 = ComplexNumber(2, 7.5)
-c3 = ComplexNumber(5)
+c1 = module.ComplexNumber(3, 4)
+c2 = module.ComplexNumber(2, 7.5)
+c3 = module.ComplexNumber(5)
 print(c1)
 print(c2)
 print(-c3)
@@ -66,4 +11,3 @@ print(c1.argument())
 print(c1+c2)
 print(c1*c2)
 print(c1.conjugate())
-
